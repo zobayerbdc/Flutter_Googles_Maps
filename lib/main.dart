f7199dab3499efb44177f143dbfd8b4f2c7bf05a
zobayerbdc <nayem.bdcit@gmail.com>
2024-11-11 16:38:12 +0600

    google location serach and get distance

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_googles_maps/distances.dart';
 import 'package:flutter_googles_maps/home_screen.dart';
+import 'package:flutter_googles_maps/polygon_area.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Google Maps',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: //const HomeMapScreen(),
-          const HomeScreen(),
+          //const HomeScreen(),
+          DistanceApp(),
     );
   }
 }
